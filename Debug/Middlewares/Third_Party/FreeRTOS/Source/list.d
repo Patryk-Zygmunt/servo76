7efc8237d172dba2a4ad9f27cc797e7db4ba9715
patryk-zygmunt <patrykzygmuntzx@gmail.com.com>
2018-06-14 21:54:02 +0200

    init

---
+++ Debug/Middlewares/Third_Party/FreeRTOS/Source/list.d
@@ -0,0 +1,29 @@
+Middlewares/Third_Party/FreeRTOS/Source/list.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/list.c \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/Linus/Documents/servo3232/Inc/FreeRTOSConfig.h \
+ C:/Users/Linus/Documents/servo3232/Inc/main.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/Linus/Documents/servo3232/Inc/FreeRTOSConfig.h:
+
+C:/Users/Linus/Documents/servo3232/Inc/main.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/Linus/Documents/servo3232/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
